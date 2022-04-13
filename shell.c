ab88d0a49073984c96ff431692188a30eddd5f41
DevMustrowk <elmustapha.abourar@outlook.com>
2022-04-13 11:11:19 +0200

    New Folder

---
+++ shell.c
@@ -0,0 +1,106 @@
+#include "shell.h"
+
+/**
+ * main - simple shell
+ * @ac: number of arguments
+ * @av: array of arguments
+ * @env: array of environment variables
+ * Return: 0 or 1 or exit value
+ */
+int main(int ac, char **av, char **env)
+{
+	size_t bufsize = 0;
+	char *buffer = NULL;
+	int nb_cmd = 1;
+
+	if (signal(SIGINT, sigintHandler) == SIG_ERR)
+		return (1);
+
+	if (!isatty(STDIN_FILENO) || !isatty(STDOUT_FILENO))
+	{
+		non_int(av[0], buffer, bufsize, nb_cmd, env);
+		return (0);
+	}
+
+	while (1)
+	{
+		inter(av[0], buffer, bufsize, nb_cmd, env);
+		nb_cmd++;
+	}
+	(void)ac;
+	return (0);
+}
+
+/**
+ * inter - execute the simple shell in normal mode
+ * @name: name of the executable
+ * @buffer: buffer receiving getline
+ * @bufsize: size of the buffer
+ * @nb_cmd: number of command executed
+ * @env: environment variables
+ */
+void inter(char *name, char *buffer, size_t bufsize, int nb_cmd, char **env)
+{
+	int nb = 0;
+
+	write(STDOUT_FILENO, "$ ", 2);
+	nb = getline(&buffer, &bufsize, stdin);
+	if (nb == -1)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		if (buffer)
+			free(buffer);
+		exit(0);
+	}
+	if (nb > 0)
+		buffer[nb - 1] = '\0';
+	if (*buffer)
+	{
+			execute_command(buffer, name, nb_cmd, env);
+	}
+
+	if (buffer)
+	{
+		free(buffer);
+		buffer = NULL;
+	}
+}
+
+/**
+ * non_int - execute the simple shell in non interactive mode
+ * @name: name of the executable
+ * @buffer: buffer receiving getline
+ * @bufsize: size of the buffer
+ * @nb_cmd: number of command executed
+ * @env: environment variables
+ */
+void non_int(char *name, char *buffer, size_t bufsize, int nb_cmd, char **env)
+{
+	int nb = 0;
+
+	while ((nb = getline(&buffer, &bufsize, stdin)) >= 0)
+	{
+		if (nb > 0)
+			buffer[nb - 1] = '\0';
+		if (*buffer != '\0')
+			execute_command(buffer, name, nb_cmd, env);
+		free(buffer);
+		buffer = NULL;
+	}
+	if (buffer)
+	{
+		free(buffer);
+		buffer = NULL;
+	}
+}
+
+/**
+ * sigintHandler - if Ctrl+C is pressed
+ * @sig: signal sent to the function
+ */
+void sigintHandler(int sig)
+{
+	write(STDOUT_FILENO, "\n", 1);
+	write(STDOUT_FILENO, "$ ", 2);
+	(void)sig;
+}
---
+++ shell.h
@@ -0,0 +1,49 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include "printf.h"
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+/**
+ * struct Token - linked list
+ * @str: string with a token of the command
+ * @next: pointer to the next node
+ */
+typedef struct Token
+{
+	char *str;
+	struct Token *next;
+} tok_t;
+
+void inter(char *, char *, size_t, int, char **);
+void non_int(char *, char *, size_t, int, char **);
+void sigintHandler(int);
+
+void execute_command(char *, char *, int, char **);
+void cmd_null(char *, char *, char **, char *, int);
+void exe_cmd(char **, char *, char **);
+void print_error(char *);
+
+int exit_value(char *);
+
+tok_t *add_node(tok_t **, char *);
+void free_list(tok_t **);
+
+char *_which(char *, char **);
+char *_getenv(const char *, char **);
+
+int _strlen(char *);
+char *_strcpy(char *, char *);
+char *_strchr(char *, char);
+char *_strcat(char *, char *);
+char *_strdup(char *);
+int _strcmp(char *, char *);
+char *_strncpy(char *, char *, int);
+char *_strtok(char *base, char *delims);
+
+#endif /* SHELL_H */
